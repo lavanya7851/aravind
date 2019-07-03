a40df87bc0243b51fd702d29f3f4e0f385ca6293
Aravind1145 <52447550+Aravind1145@users.noreply.github.com>
2019-07-03 09:52:01 +0530

    Create a3.py

---
+++ a3.py
@@ -0,0 +1,9 @@
+ch = int(input())
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
+       or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
+       	print("Vowel")
+elif (ch == '$' or a == '&'):
+	print("invalid")
+else:
+	print("Consonant")
+	
